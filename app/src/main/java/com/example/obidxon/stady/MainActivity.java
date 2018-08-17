2680b6eaf1d1702b72d4f08755c3516a2db2ef9e
Obidxon <qoraxojayev2408@gmail.com>
2018-08-18 02:13:24 +0500

    Toast commit

--- app/src/main/java/com/example/obidxon/stady/MainActivity.java
+++ app/src/main/java/com/example/obidxon/stady/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.obidxon.stady;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this,"Commit",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
     }
 }
